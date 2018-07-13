f186123cf447f6057b60ed2d47223ce8a81a9b8a
unknown <kqpham007@gmail.com>
2018-07-13 10:48:02 -0700

    ..

---
+++ public/src/app/http.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+	constructor( private _http: HttpClient ) {
+  	this.getTask();
+  	this.getShow();
+  	this.getDelete();
+	}
+	getTask(){
+		
+		const observable = this._http.get('/task');
+		observable.subscribe(data => {
+			console.log("Got our tasks!", data)
+		});
+	}
+	getShow(id = "5b4793f023d58a12a8f69546"){
+		
+		const observable = this._http.get('/task/' + id);
+		observable.subscribe(data => {
+			console.log("Tasks with id!", data)
+		});
+	}
+	getDelete(id = "5b4793f023d58a12a8f69546"){
+		
+		const observable = this._http.delete('/task/' + id);
+		observable.subscribe(data => {
+			console.log("DELETE THIS TASK!", data)
+		});
+	}
+}
---
+++ server.js
@@ -0,0 +1,83 @@
+path = require('path');
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use( bodyParser.urlencoded({ extended: true }));
+app.use( bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public/dist/public')));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/taskdb', { useNewUrlParser: true });
+
+const TaskSchema = new mongoose.Schema({
+	title: { type: String, required: true },
+	description: { type: String, default: ''},
+	completed: { type: Boolean, default: false}
+}, { timestamps: true });
+var Task = mongoose.model("Task", TaskSchema);
+
+app.get('/task', function(req, res){
+	Task.find({}, function(err, task){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.json({ task });
+		}
+	})
+})
+
+app.post('/task', function(req, res){
+	var task = new Task();
+	task.title = req.body.title;
+	task.description = req.body.description;
+	task.save(function(err, task){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.json({ task });
+		}
+	})
+})
+
+app.get('/task/:id', function(req, res){
+	Task.findOne({_id: req.params.id }, function(err, task){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.json({ task });
+		}
+	})
+})
+
+app.put('/task/:id', function(req, res){
+	var task = Task.findOne({ _id: req.params.id }, function(err, task){
+		task.title = req.body.title;
+		task.description = req.body.description;
+		task.save(function(err, task){
+			if(err){
+				console.log(err);
+			}
+			else{
+				res.json({ task });
+			}
+		})
+	})
+})
+app.delete('/task/:id', function(req, res){
+	Task.findOneAndDelete( {_id: req.params.id }, function(err, task){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.json({ task });
+		}
+	})
+})
+
+app.listen(1337);
